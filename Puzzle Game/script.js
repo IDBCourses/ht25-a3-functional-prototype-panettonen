a674c179c4a07ba92b3e4227d0d8181a9d802f6d
Timothy <115110343+panettonen@users.noreply.github.com>
2025-10-28 22:27:49 +0100

    Now it's possible to move around the colors on the cube with the keyboard. + Also changed how the cubes change color by setting the color first on the objects color property. Then draw it by applying the color to the elements style background color. + Added a function to reset to the default color position

--- Puzzle Game/script.js
+++ Puzzle Game/script.js
@@ -9,6 +9,23 @@ let leftKey = false;
 let rightKey = false;
 let centerKey = false;
 
+const moves = {
+  upLeft: false,
+  upRight: false,
+  downLeft: false,
+  downRight: false,
+
+  leftUp: false,
+  leftDown: false,
+  rightUp: false,
+  rightDown: false,
+
+  centerUp: false,
+  centerDown: false,
+  centerLeft: false,
+  centerRight: false
+};
+
 let keys = [
   ["KeyQ", "KeyA", "KeyZ"],
   ["KeyW", "KeyS", "KeyX"],
@@ -31,9 +48,81 @@ const colors = {
 
 // Code that runs over and over again
 function loop() {
-  // cubes[0][2].el.style.backgroundColor = colors.orange;
+  moveColors();
+  drawCubes();
 
   window.requestAnimationFrame(loop);
+}
+
+function moveColors() {
+  // Up
+  if (moves.upLeft) {
+    move(2, 0, 1, 0, 0, 0);
+    moves.upLeft = false;
+  } else if (moves.upRight) {
+    move(0, 0, 1, 0, 2, 0);
+    moves.upRight = false;
+  }
+  
+  // Down
+  if (moves.downLeft) {
+    move(2, 2, 1, 2, 0, 2);
+    moves.downLeft = false;
+  } else if (moves.downRight) {
+    move(0, 2, 1, 2, 2, 2);
+    moves.downRight = false;
+  }
+
+  // Left
+  if (moves.leftUp) {
+    move(0, 2, 0, 1, 0, 0);
+    moves.leftUp = false;
+  } else if (moves.leftDown) {
+    move(0, 0, 0, 1, 0, 2);
+    moves.leftDown = false;
+  }
+
+  // Right
+  if (moves.rightUp) {
+    move(2, 2, 2, 1, 2, 0);
+    moves.rightUp = false;
+  } else if (moves.rightDown) {
+    move(2, 0, 2, 1, 2, 2);
+    moves.rightDown = false;
+  }
+
+  // Center
+  if (moves.centerUp) {
+    move(1, 2, 1, 1, 1, 0);
+    moves.centerUp = false;
+  } else if (moves.centerDown) {
+    move(1, 0, 1, 1, 1, 2);
+    moves.centerDown = false;
+  } else if (moves.centerLeft) {
+    move(2, 1, 1, 1, 0, 1);
+    moves.centerLeft = false;
+  } else if (moves.centerRight) {
+    move(0, 1, 1, 1, 2, 1);
+    moves.centerRight = false;
+  }
+}
+
+function move(x1, y1, x2, y2, x3, y3) {
+  let temp1 = cubes[x1][y1].color;
+  let temp2 = cubes[x2][y2].color;
+  let temp3 = cubes[x3][y3].color;
+
+  cubes[x1][y1].color = temp3;
+  cubes[x2][y2].color = temp1;
+  cubes[x3][y3].color = temp2;
+}
+
+function drawCubes() {
+  for (let x = 0; x < 3; x++) {
+    for (let y = 0; y < 3; y++) {
+      cubes[x][y].el.style.backgroundColor = cubes[x][y].color;
+    }
+  }
 }
 
 
@@ -41,9 +130,7 @@ function createCubeObjects() {
   for (let i = 1; i <= 9; i++) {
     cubeObjects[`c${i}`] = {
       el: document.getElementById(`cube${i}`),
-      color: "",
-      x: 0,
-      y: 0
+      color: ""
     }
   }
 }
@@ -63,8 +150,14 @@ function setCubesObjects() {
 
 function setCubesColor(row, color) {
   for (let i = 0; i < 3; i++) {
-    cubes[i][row].el.style.backgroundColor = color;
+    cubes[i][row].color = color;
   }
+}
+
+function resetCubesColor() {
+  setCubesColor(0, colors.orange);
+  setCubesColor(1, colors.green);
+  setCubesColor(2, colors.purple);
 }
 
 function keyPressed(event) {
@@ -74,9 +167,9 @@ function keyPressed(event) {
   }
   if (upKey == true) {
     if (event.code == keys[0][0]) { // Up - Left
-      console.log("Up - Left")
+      moves.upLeft = true;
     } else if (event.code == keys[2][0]) { // Up - Right
-      console.log("Up - Right")
+      moves.upRight = true;
     }
   }
 
@@ -86,9 +179,9 @@ function keyPressed(event) {
   }
   if (downKey == true) {
     if (event.code == keys[0][2]) { // Down - Left
-      console.log("Down - Left")
+      moves.downLeft = true;
     } else if (event.code == keys[2][2]) { // Down - Right
-      console.log("Down - Right")
+      moves.downRight = true;
     }
   }
 
@@ -98,9 +191,9 @@ function keyPressed(event) {
   }
   if (leftKey == true) {
     if (event.code == keys[0][0]) { // Left - Up
-      console.log("Left - Up")
+      moves.leftUp = true;
     } else if (event.code == keys[0][2]) { // Left - Down
-      console.log("Left - Down")
+      moves.leftDown = true;
     }
   }
 
@@ -110,9 +203,9 @@ function keyPressed(event) {
   }
   if (rightKey == true) {
     if (event.code == keys[2][0]) { // Right - Up
-      console.log("Right - Up")
+      moves.rightUp = true;
     } else if (event.code == keys[2][2]) { // Right - Down
-      console.log("Right - Down")
+      moves.rightDown = true;
     }
   }
 
@@ -122,13 +215,13 @@ function keyPressed(event) {
   }
   if (centerKey == true) {
     if (event.code == keys[1][0]) { // Center - Up
-      console.log("Center - Up")
+      moves.centerUp = true;
     } else if (event.code == keys[1][2]) { // Center - Down
-      console.log("Center - Down")
+      moves.centerDown = true;
     } else if (event.code == keys[0][1]) { // Center - Left
-      console.log("Center - Left")
+      moves.centerLeft = true;
     } else if (event.code == keys[2][1]) { // Center - Right
-      console.log("Center - Right")
+      moves.centerRight = true;
     }
   }
 }
@@ -167,9 +260,7 @@ function setup() {
   ];
 
   setCubesObjects();
-  setCubesColor(0, colors.orange);
-  setCubesColor(1, colors.green);
-  setCubesColor(2, colors.purple);
+  resetCubesColor();
 
   document.addEventListener("keydown", keyPressed);
   document.addEventListener("keyup", keyReleased);
